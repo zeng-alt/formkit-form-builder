f6d6d05a4edc5cd1076cdc33097d6d894786e1ba
zeng-alt <zeng-alt@users.noreply.github.com>
2026-04-14 08:48:30 +0000

    feat: 生成项目Code Wiki文档
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/builder/BuilderMain.vue
+++ src/builder/BuilderMain.vue
@@ -1,20 +1,38 @@
 <script setup lang="ts">
-import { NLayout } from "naive-ui";
+import {
+  NConfigProvider,
+  NLayout,
+  type ConfigProviderProps
+} from "naive-ui";
 import SidebarLeft from "../components/sidebar-left/SidebarLeft.vue";
 import SidebarRight from "../components/sidebar-right/SidebarRight.vue";
 import BuilderDropArea from "./BuilderDropArea.vue";
 import BuilderHeader from "./BuilderHeader.vue";
+
+defineProps<ConfigProviderProps>();
 </script>
 
 <template>
-  <n-layout has-sider class="h-screen w-full">
-    <SidebarLeft />
-    <n-layout class="relative bg-gradient-to-br from-secondary to-emerald-100/50 dark:from-secondary dark:to-emerald-800/20" :native-scrollbar="false">
-      <div class="p-4 h-full flex flex-col">
-        <BuilderHeader />
-        <BuilderDropArea />
-      </div>
+  <n-config-provider
+    :theme="theme"
+    :theme-overrides="themeOverrides"
+    :locale="locale"
+    :date-locale="dateLocale"
+    :breakpoints="breakpoints"
+    :cls-prefix="clsPrefix"
+    :abstract="abstract"
+    :inline-theme-disabled="inlineThemeDisabled"
+    :preflight-style-disabled="preflightStyleDisabled"
+  >
+    <n-layout has-sider class="h-screen w-full">
+      <SidebarLeft />
+      <n-layout class="relative bg-gradient-to-br from-secondary to-emerald-100/50 dark:from-secondary dark:to-emerald-800/20" :native-scrollbar="false">
+        <div class="p-4 h-full flex flex-col">
+          <BuilderHeader />
+          <BuilderDropArea />
+        </div>
+      </n-layout>
+      <SidebarRight />
     </n-layout>
-    <SidebarRight />
-  </n-layout>
+  </n-config-provider>
 </template>
